9e46738f220feaceda67bf9722d582d8b7bed8d6
Tiya Gallegos <tiyag@Tiyas-MacBook-Pro.local>
2020-06-06 15:54:23 -0600

    created setup schema and embedded fs schema

---
+++ models/setup.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const feedingSessionSchema = new Schema({
+  date: Date,
+  lengthOfSession: Number,
+  createdBy: ObjectId,
+  setUp: ObjectId, 
+  perOral: String,
+  aspiration: Boolean,
+  volume: Number,
+  exploredFoods: Boolean,
+  description: String
+
+}, { timestamps:true});
+
+const setUpSchema = new Schema({
+  seating: Boolean,
+  createdBy: ObjectId,
+  foodPrep: Boolean,
+  transition: Boolean, 
+  tools: Boolean,
+  comments: String,
+  feedingSessions: [ feedingSessionSchema],
+  
+}, {
+  timestamps: true
+});
+
+module.exports = mongoose.model('Movie', movieSchema);
