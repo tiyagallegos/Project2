67fcc96616eccac0028705d640ae4f47553054f9
Tiya Gallegos <tiyag@Tiyas-MacBook-Pro.local>
2020-06-19 14:11:18 -0600

    config db

--- config/database.js
+++ config/database.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 
 
-mongoose.connect(process.env.DATABASE_URL, {
+mongoose.connect(process.env.DATABASE_URL || 'mongodb:/localhost/users', {
 	useNewUrlParser: true,
 	useUnifiedTopology: true,
 	useCreateIndex: true
